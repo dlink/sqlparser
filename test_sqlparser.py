a02fc6757b66f5b1a23dd2979b73fb636bd15d29
David Link <dvlink@gmail.com>
2015-04-20 12:16:15 -0400

    Unit Tests: Refactored

--- test_sqlparser.py
+++ test_sqlparser.py
@@ -15,45 +15,31 @@ SELECT4 = 'select b.id, b.name from books b'
 class TestSqlParser(unittest.TestCase):
 
     def testSql(self):
-        p = SqlParser()
-        results = p.parse(SELECT)
-        self.assertEqual(
-            str(['select', '*', 'from', 'books']),
-            str(results))
-        self.assertEqual(results.select_clause, '*')
-        self.assertEqual(list(results.from_clause), ['books'])
+        self._process(SELECT, ['select', '*', 'from', 'books'], '*', ['books'])
 
     def testSql2(self):
-        p = SqlParser()
-        results = p.parse(SELECT2)
-        self.assertEqual(
-            str(['select', [['id']], 'from', 'books']),
-            str(results))
-        self.assertEqual(
-            str(results.select_clause),
-            str([['id']]))
-        self.assertEqual(list(results.from_clause), ['books'])
+        self._process(SELECT2,
+                     ['select', [['id']], 'from', 'books'],
+                     [['id']],
+                     ['books'])
 
     def testSql3(self):
-        p = SqlParser()
-        results = p.parse(SELECT3)
-        self.assertEqual(
-            str(['select', [['id'], ['name']], 'from', 'books']),
-            str(results))
-        self.assertEqual(
-            str(results.select_clause),
-            str([['id'], ['name']]))
-        self.assertEqual(list(results.from_clause), ['books'])
+        self._process(SELECT3,
+                     ['select', [['id'], ['name']], 'from', 'books'],
+                     [['id'], ['name']],
+                     ['books'])
 
     def testSql4(self):
+        self._process(SELECT4,
+                      ['select', [['b.id'], ['b.name']], 'from', 'books', 'b'],
+                      [['b.id'], ['b.name']],
+                      ['books', 'b'])
+
+    def _process(self, sql, parsed_sql, select_clause, from_clause):
         p = SqlParser()
-        results = p.parse(SELECT4)
-        self.assertEqual(
-            str(['select', [['b.id'], ['b.name']], 'from', 'books', 'b']),
-            str(results))
-        self.assertEqual(
-            str(results.select_clause),
-            str([['b.id'], ['b.name']]))
-        self.assertEqual(list(results.from_clause), ['books', 'b'])
+        results = p.parse(sql)
+        self.assertEqual(str(results), str(parsed_sql))
+        self.assertEqual(str(results.select_clause), str(select_clause))
+        self.assertEqual(list(results.from_clause), from_clause)
 
 unittest.main()
